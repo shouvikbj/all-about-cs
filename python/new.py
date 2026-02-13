d49d9ef3d1ea3ac54b8cfbb40b2d6490e8aa6647
Anasmita-Math <mathanasmita@gmail.com>
2026-02-13 15:34:04 +0530

    Basic operations

--- python/new.py
+++ python/new.py
@@ -1,3 +1,19 @@
 a = 90
 print(a)
-# ctrl+j
+# Basics 
+name=input("Enter the value of a:")
+print(name)
+# Calculation
+first_num=234
+second_num=34
+res=first_num//second_num
+res=first_num%second_num
+print(res)
+print(type(res))
+# Simple Interest
+p=int(input("Enter p: "))
+r=int(input("Enter r: "))
+t=int(input("Enter t: "))
+si=(p*r*t)/100
+print(si)
+type(si)
