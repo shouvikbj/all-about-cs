55877b89a9dacd7a0be21da9d736e200834e8b2e
Anasmita-Math <mathanasmita@gmail.com>
2026-02-13 16:05:24 +0530

    Conditional statements

--- python/new.py
+++ python/new.py
@@ -1,19 +1,37 @@
-a = 90
-print(a)
-# Basics 
-name=input("Enter the value of a:")
-print(name)
-# Calculation
-first_num=234
-second_num=34
-res=first_num//second_num
-res=first_num%second_num
-print(res)
-print(type(res))
-# Simple Interest
-p=int(input("Enter p: "))
-r=int(input("Enter r: "))
-t=int(input("Enter t: "))
-si=(p*r*t)/100
-print(si)
-type(si)
+# a = 90
+# print(a)
+# # Basics 
+# name=input("Enter the value of a:")
+# print(name)
+# # Calculation
+# first_num = 234
+# second_num = 34
+# res=first_num // second_num
+# res=first_num % second_num
+# print(res)
+# print(type(res))
+# # Simple Interest
+# p = int(input("Enter p: "))
+# r = int(input("Enter r: "))
+# t = int(input("Enter t: "))
+# si=(p*r*t)/100
+# print(si)
+# type(si)
+# Conditional Statements (if-else)
+# ODD-EVEN
+n = 29
+if (n % 2 ==0):
+    print("The number is even")
+else:
+    print("The number is odd")
+# LARGEST AMONG THREE
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+if (a>b and a>c):
+    print("a is the largest")
+elif (b>a and b>c):
+    print("b is the largest")
+else:
+    print("c is the largest")
+    
