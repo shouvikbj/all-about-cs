d81ada119823424f33a3f1cea5fbe1adc29af3e5
Anasmita-Math <mathanasmita@gmail.com>
2026-02-15 11:50:03 +0530

    My first JavaScript Commit

---
+++ javascript/new_js.js
@@ -0,0 +1,23 @@
+// ||Varibles|| \\
+// Using Var
+var age=20;
+var age=24;
+var age=30;
+console.log(age);
+let name="Anasmita Math";
+// Using Let
+// let name="Smita"; It gives error 
+console.log(name);
+name="Pupu"; // Update
+console.log(name);
+// Using Const
+const pi=3.14;
+// pi=4.5; //Runtime Error
+console.log(pi);
+// Mixed Value
+const num1 = 23;
+const num2=34;
+let total = num1 + num2;
+console.log(total);
+let sub = num1 - num2;
+console.log(sub);
