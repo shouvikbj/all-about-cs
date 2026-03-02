32cf54eafa90ec5b50419f6cfc2ad2ad12b45bb2
Anasmita-Math <mathanasmita@gmail.com>
2026-03-03 02:21:12 +0530

    Jinja templating

--- first_web_app/app.py
+++ first_web_app/app.py
@@ -33,9 +33,18 @@ def dynamic_with_age(email):
     )
 
 @app.route('/details/<first_name>/<last_name>/dept/<dept_name>/college/<college_name>')
-def details():
+def details(first_name,last_name,dept_name,college_name):
     # TODO: provide your implementation
-    return render_template('details.html')
+    fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+    cap_fruits = [fruit.capitalize() for fruit in fruits]
+    return render_template(
+        'details.html',
+        first_n=first_name.capitalize(), 
+        last_n=last_name.capitalize(),
+        dept_n=dept_name.upper(),
+        college_n=college_name.upper(),
+        fruits=cap_fruits
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
